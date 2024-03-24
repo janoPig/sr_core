745da9fd8f1dfdb754160992850760e2b5e7a6a4
janoPig <jan.pigos@gmail.com>
2024-03-24 12:12:37 +0100

    libmvec

--- Hroch/clang.sh
+++ Hroch/clang.sh
@@ -3,6 +3,6 @@
 clear
 echo buid...
 
-clang++-15 *.cpp -o ./bin/hroch.bin -DNDEBUG -fveclib=SVML -std=c++20 -O3 -mavx2 -Wall -Wextra -funsafe-math-optimizations -ftree-vectorize -fno-exceptions -shared -fPIC #-fsanitize=address
+clang++-15 *.cpp -o ./bin/hroch.bin -DNDEBUG -fveclib=libmvec -std=c++20 -O3 -mavx2 -Wall -Wextra -fno-math-errno -fno-signed-zeros -funsafe-math-optimizations -ftree-vectorize -fno-exceptions -shared -fPIC #-fsanitize=address
 
 echo done.
