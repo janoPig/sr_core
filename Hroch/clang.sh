15fd35cd248d3b853583dc18daac2cdc215f1d67
Jan Pigos <jan.pigos@gmail.com>
2025-08-17 14:00:38 +0200

    linker -v

--- Hroch/clang.sh
+++ Hroch/clang.sh
@@ -3,6 +3,6 @@
 clear
 echo buid...
 
-clang++-18 *.cpp -o ./bin/hroch.bin -DNDEBUG -fveclib=libmvec -std=c++20 -O3 -mavx2 -Wall -Wextra -fno-math-errno -fno-signed-zeros -funsafe-math-optimizations -ftree-vectorize -fno-exceptions -shared -fPIC #-fsanitize=address
+clang++-18 *.cpp -o ./bin/hroch.bin -DNDEBUG -fveclib=libmvec -std=c++20 -O3 -mavx2 -Wall -Wextra -fno-math-errno -fno-signed-zeros -funsafe-math-optimizations -ftree-vectorize -fno-exceptions -shared -fPIC -v #-fsanitize=address
 
 echo done.
